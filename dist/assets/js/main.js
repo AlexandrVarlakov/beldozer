9c5321ab63cf9fff4ad1753c38dd508371445430
AlexandrVarlakov <66496459+AlexandrVarlakov@users.noreply.github.com>
2022-07-23 15:17:12 +0300

    create menu for desktop

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -32,3 +32,66 @@ let slider = new Swiper(".i-slider", {
         },
     },
 });
+
+
+
+let showContactsBtn = document.querySelector('.show-contacts');
+let contactsMenu = document.querySelector('.contact-menu');
+
+
+showContactsBtn.onclick = function(){
+    if ( !this.classList.contains('open')){
+        this.classList.add('open');
+        contactsMenu.classList.add('open');
+        document.body.classList.add('hide-scroll');
+    } else{
+        this.classList.remove('open');
+        contactsMenu.classList.remove('open');
+        document.body.classList.remove('hide-scroll');
+    }
+}
+
+
+let dropMenuItem = document.querySelectorAll('.dm-links-list__item');
+
+dropMenuItem.forEach(item => {
+    item.addEventListener( 'mouseenter', (event)=>{
+
+       if ( !event.target.classList.contains('open') && event.target.classList.contains('has-child')){
+        let openedItem = document.querySelector('.dm-links-list__item.has-child.open');
+
+        if ( openedItem ){
+            openedItem.classList.remove('open');
+        }
+
+
+        event.target.classList.add('open');
+       } else if (!event.target.classList.contains('open') && !event.target.classList.contains('has-child')) {
+            let openedItem = document.querySelector('.dm-links-list__item.has-child.open');
+            if ( openedItem ){
+                openedItem.classList.remove('open');
+            }
+       }
+    })
+})
+
+
+let dmLinksContainer = document.querySelectorAll('.dm-links-container');
+
+if ( dmLinksContainer.length ){
+    dmLinksContainer.forEach( container => {
+
+        container.onmouseleave = function(){
+            let openedItems =  this.querySelectorAll('.dm-links-list__item.has-child.open');
+
+            if ( openedItems.length ){
+                openedItems.forEach( item => {
+                    item.classList.remove('open');
+                } )
+            }
+        }
+    
+    } )
+}
+
+
